fb18673a68c15a933819f4ed80c6059295f76b7c
Siddharth Lal Deo <41829093+siddharthdeo99@users.noreply.github.com>
2020-10-31 00:18:26 +0530

    Update ytbot.py

--- ytbot.py
+++ ytbot.py
@@ -1,21 +1,21 @@
 from selenium import webdriver
 from time import sleep
 
-driver1= webdriver.Chrome(executable_path="C:\Users\siddh\Downloads\Compressed\chromedriver_win32\chromedriver")#In executable path give the path of chromedriver that you have downloaded. In my case it is in downloads.
-driver2= webdriver.Chrome(executable_path="C:\Users\siddh\Downloads\Compressed\chromedriver_win32\chromedriver")
-driver3= webdriver.Chrome(executable_path="C:\Users\siddh\Downloads\Compressed\chromedriver_win32\chromedriver")
+siddharth1= webdriver.Chrome(executable_path="C:\Users\siddh\Downloads\Compressed\chromedriver_win32\chromedriver")#In executable path give the path of chromedriver that you have downloaded. In my case it is in downloads.
+siddharth2= webdriver.Chrome(executable_path="C:\Users\siddh\Downloads\Compressed\chromedriver_win32\chromedriver")
+siddharth3= webdriver.Chrome(executable_path="C:\Users\siddh\Downloads\Compressed\chromedriver_win32\chromedriver")
 
-driver1.get("https://youtu.be/OU69JaZ1FQo")#provide the link
-driver2.get("https://youtu.be/OU69JaZ1FQo")
-driver3.get("https://youtu.be/OU69JaZ1FQo")
-#i have used driver 1,2,3  so it will open 3 tabs 
+siddharth1.get("https://youtu.be/OU69JaZ1FQo")#provide the link
+siddharth2.get("https://youtu.be/OU69JaZ1FQo")
+siddharth3.get("https://youtu.be/OU69JaZ1FQo")
+#i have used siddharth 1,2,3  so it will open 3 tabs 
 #if you want to add more tabs you can also do it 
 
 while True:
     sleep(18)#sleep 18 meanes after 18 seconds page will refresh
-    driver1.refresh()
-    driver2.refresh()
-    driver3.refresh()
+    siddharth1.refresh()
+    siddharth2.refresh()
+    siddharth3.refresh()
     
     #To execute - Simple run this code
     #Happy coding - Siddharth
